16c526558dbc52e600a6ebfb7ffc9762bae83f9c
Zach Bjornson <bjornson@stanford.edu>
2016-02-06 23:52:39 -0800

    Update runner.js

--- lambdas/lambda1/runner.js
+++ lambdas/lambda1/runner.js
@@ -4,29 +4,20 @@
 
 var childProcess = require('child_process');
 
-exports.handler = function (event, context) {
-	var child = childProcess.spawn("./node/bin/node", ["./myfunction.js", JSON.stringify(event)], {
-		stdio: ["pipe", "pipe", "pipe", "ipc"]
-	});
+var child = childProcess.spawn("./node/bin/node", ["./myfunction.js"], {
+	stdio: [process.stdin, process.stdout, process.stderr, "ipc"]
+});
 
-  // Channel the result from the worker back to Lambda
+exports.handler = function (event, context) {
+	child.send(event);
+
+	// Channel the result from the worker back to Lambda
 	child.on("message", function (msg) {
 		context.done(null, msg);
 	});
 
-  // Channel errors
+	// Channel errors
 	child.on("error", function (msg) {
 		context.done(msg);
 	});
-
-  // Other STDIO
-	// Thought these wouldn't be necessary because of stdio inheritance in spawn,
-	// but they seem to be.
-	child.stdout.on("data", function (m) {
-		console.log(m.toString());
-	});
-	child.stderr.on("data", function (m) {
-		console.error(m.toString());
-	});
-
 };
