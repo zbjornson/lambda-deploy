e35e6a37911cdfbdfce0d8ccd92a7814709d43ab
Zach Bjornson <bjornson@stanford.edu>
2016-02-06 23:52:42 -0800

    Update myfunction.js

--- lambdas/lambda1/myfunction.js
+++ lambdas/lambda1/myfunction.js
@@ -1,9 +1,7 @@
-// Debug: console.error("Job: " + process.argv[2]);
-// This is the IPC channel input
-var event = JSON.parse(process.argv[2]);
+process.on("message", function (event) {
+	// Do work.
+	var result = "42";
 
-// Do work.
-var result = "42";
-
-// Send the result back
-process.send(result);
+	// Send the result back
+	process.send(result);
+})
